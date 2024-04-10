f4289924b2dc8f29056b89343087586170eff838
Shishir <shishiradhikari2060@gmail.com>
2024-04-10 05:08:47 +0100

    Added linkedinlist.h

---
+++ linkedlist.h
@@ -0,0 +1,11 @@
+using namespace std;
+
+class Node{
+    public:
+        int info;
+        Node * next;
+
+        Node(){}
+        Node(int d):info(d), next(nullptr) {}
+        Node (int d, Node * next) : info(d), next(next) {}
+};
