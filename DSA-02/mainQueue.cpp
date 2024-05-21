316d31c1520a513e914fd5261c7794f5f9be7bb1
Shishir <shishiradhikari2060@gmail.com>
2024-05-21 22:59:13 +0545

    array also completed

--- DSA-02/mainQueue.cpp
+++ DSA-02/mainQueue.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
-    IQueue *Queue = new LinkedListQueue;
+    IQueue *Queue = new ArrayQueue(5);
     int data;
-    Queue->enQueue(5);
+    Queue->enQueue(2);
     Queue->enQueue(6);
-    Queue->enQueue(7);
+    Queue->front(data);
+    std::cout<<"The data at front is : "<<data<<std::endl;
+    Queue->enQueue(9);
     Queue->traverse();
     Queue->deQueue(data);
     std::cout<<"\nThe deQueued data is : "<<data<<std::endl;
